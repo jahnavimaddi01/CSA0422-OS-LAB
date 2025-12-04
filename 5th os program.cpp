46ad0cd5892d0b55a51afcb0f3d4d99029e072c4
jahnavimaddi01 <maddijanvi@gmail.com>
2025-12-04 12:45:36 +0530

    Add files via upload

---
+++ 5th os program.cpp
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+int main() {
+    int n,i,j,t;
+    printf("Enter number of processes: ");
+    scanf("%d",&n);
+
+    int bt[n],p[n];
+
+    for(i=0;i<n;i++){
+        printf("Enter burst time of P%d: ",i+1);
+        scanf("%d",&bt[i]);
+        p[i]=i+1;
+    }
+
+    for(i=0;i<n;i++)
+        for(j=i+1;j<n;j++)
+            if(bt[i] > bt[j]){
+                t=bt[i]; bt[i]=bt[j]; bt[j]=t;
+                t=p[i];  p[i]=p[j];  p[j]=t;
+            }
+
+    printf("\nExecution order: ");
+    for(i=0;i<n;i++) printf("P%d ",p[i]);
+}
+
